a2ea6f0ef45521e5737c77248205ada65e217a85
Vinicius Muller de Freitas <viniciusmullerdefreitas309@gmail.com>
2024-08-26 01:24:46 -0300

    create resp command and add informations aabout wather breathing

--- main.py
+++ main.py
@@ -16,12 +16,8 @@ intents.message_content = True
 # Instanciete the bot
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-def load_effect():
-    with open('negative_effect.json', 'r') as f:
-        return json.load(f)
-
-def load_effect2():
-    with open('positive_effect.json', 'r') as f:
+def load_effect(file_name):
+    with open(file_name, 'r') as f:
         return json.load(f)
 
 @bot.event
@@ -36,7 +32,7 @@ async def pong(ctx):
 # Negative effects
 @bot.command()
 async def debuff(ctx, *, type_name):
-    effect_info = load_effect()
+    effect_info = load_effect("negative_effect.json")
     normalized_name = unidecode(type_name.lower())
     if normalized_name in effect_info:
         await ctx.reply(effect_info[normalized_name.lower()])
@@ -46,14 +42,30 @@ async def debuff(ctx, *, type_name):
 # Positive Effects
 @bot.command()
 async def buff(ctx, *, type_name):
-    effect_info = load_effect2()
+    effect_info = load_effect("positive_effect.json")
     normalized_name = unidecode(type_name.lower())
     if normalized_name in effect_info:
         await ctx.reply(effect_info[normalized_name.lower()])
     else:
         await ctx.reply(f'Efeito desconhecido: {type_name}')
-        
 
+# Resp
+@bot.command()
+async def resp(ctx, type_name, technique_number):
+    resp_info = load_effect("resp.json")
+    normalized_name = unidecode(type_name.lower())
+    if normalized_name in resp_info:
+        if technique_number:
+            technique_key = str(technique_number)
+            if technique_key in resp_info[normalized_name]:
+                await ctx.reply(resp_info[normalized_name][technique_key])
+            else:
+                await ctx.reply(f'Técnica {technique_number} desconhecida para {type_name}.')
+        else:
+            # Responde com a informação completa da respiração
+            await ctx.reply(json.dumps(resp_info[normalized_name], ensure_ascii=False, indent=2))
+    else:
+        await ctx.reply(f'Resp desconhecido: {type_name}')
     
 # Run the bot
 bot.run(TOKEN)
